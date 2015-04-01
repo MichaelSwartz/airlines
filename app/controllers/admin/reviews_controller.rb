ab40fc6120ac1c5008c15761a73f649022e81d73
Michael Swartz <mjswartz@gmail.com>
2015-04-01 16:42:45 -0400

    adds admin ability to delete reviews

---
+++ app/controllers/admin/reviews_controller.rb
@@ -0,0 +1,24 @@
+module Admin
+  class ReviewsController < ApplicationController
+    before_action :authorize_admin!
+
+    def destroy
+      @review = Review.find(params[:format])
+      @airline = @review.airline
+
+      if @review.destroy
+        flash[:notice] = "The review has been deleted"
+      else
+        flash[:notice] = "Failed to delete"
+      end
+
+      redirect_to admin_airline_path(@airline)
+    end
+
+    protected
+
+    def review_params
+    end
+
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -10,19 +10,20 @@ require 'faker'
 airlines = []
 users = []
 
+20.times do
+  users << User.create_with(
+    password: Faker::Lorem.characters(10)).
+    find_or_create_by(email: Faker::Internet.email)
+end
+
 60.times do
   airlines << Airline.find_or_create_by(
     name: Faker::Company.name,
     link_url: Faker::Internet.url,
     logo_url: Faker::Company.logo,
-    description: Faker::Company.bs
+    description: Faker::Company.bs,
+    user_id: users.sample.id
   )
-end
-
-20.times do
-  users << User.create_with(
-    password: Faker::Lorem.characters(10)).
-    find_or_create_by(email: Faker::Internet.email)
 end
 
 10000.times do
@@ -33,3 +34,5 @@ end
     airline_id: airlines.sample.id
   )
 end
+
+User.create(password: "password", email: "admin@email.com", admin: true)
