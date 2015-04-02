f5099c2746fb777ecd7e740c170f9a6549d6b67a
David Tsang <davidytsang@gmail.com>
2015-04-02 12:12:45 -0400

    updated indents

--- spec/features/airlines/creating_airline_spec.rb
+++ spec/features/airlines/creating_airline_spec.rb
@@ -2,64 +2,61 @@ require 'rails_helper'
 
 feature "creating an airline" do
 
-   context 'as an authorized user' do
-     let(:user) { FactoryGirl.create(:user) }
+  context 'as an authorized user' do
+    let(:user) { FactoryGirl.create(:user) }
 
-     before :each do
-       sign_in_as user
-     end
+    before :each do
+      sign_in_as user
+    end
 
-      scenario "succesfully creates airline" do
+    scenario "succesfully creates airline" do
+      visit root_path
+      click_on "Create New Airline"
 
-        visit root_path
-        click_on "Create New Airline"
+      fill_in "Name", with: "Example Airline"
+      fill_in "Link URL", with: "www.example.com"
+      fill_in "Logo URL", with: "http://img4.wikia.nocookie.net/__cb20110201093102/logopedia/images/f/f5/Logo1929.gif"
+      click_on "Add Airline"
 
-        fill_in "Name", with: "Example Airline"
-        fill_in "Link URL", with: "www.example.com"
-        fill_in "Logo URL", with: "http://img4.wikia.nocookie.net/__cb20110201093102/logopedia/images/f/f5/Logo1929.gif"
-        click_on "Add Airline"
+      expect(page).to have_content("New Airline Added")
+      expect(page).to have_content("Example Airline")
+      expect(page).to have_link('Go to Airline Website', href: 'www.example.com')
+      expect(page).to have_xpath("//img[contains(@src,\"http://img4.wikia.nocookie.net/__cb20110201093102/logopedia/images/f/f5/Logo1929.gif\")]")
+    end
 
-        expect(page).to have_content("New Airline Added")
-        expect(page).to have_content("Example Airline")
-        expect(page).to have_link('Go to Airline Website', href: 'www.example.com')
-        expect(page).to have_xpath("//img[contains(@src,\"http://img4.wikia.nocookie.net/__cb20110201093102/logopedia/images/f/f5/Logo1929.gif\")]")
+    scenario "fails to create airline" do
 
-      end
+      visit root_path
+      click_on "Create New Airline"
 
-      scenario "fails to create airline" do
+      fill_in "Name", with: ""
+      fill_in "Link URL", with: "example.com"
+      fill_in "Logo URL", with: "example-logo.com"
+      click_on "Add Airline"
+      expect(page).to have_content("Name can't be blank")
+    end
 
-        visit root_path
-        click_on "Create New Airline"
+    scenario "user attempts to create duplicate airline, but the code says oh no you ditint" do
+      FactoryGirl.create(:airline, name: 'Delta Airlines')
 
-        fill_in "Name", with: ""
-        fill_in "Link URL", with: "example.com"
-        fill_in "Logo URL", with: "example-logo.com"
-        click_on "Add Airline"
-        expect(page).to have_content("Name can't be blank")
-      end
+      visit root_path
+      click_on "Create New Airline"
 
-      scenario "user attempts to create duplicate airline, but the code says oh no you ditint" do
-        FactoryGirl.create(:airline, name: 'Delta Airlines')
+      fill_in "Name", with: "Delta Airlines"
+      fill_in "Link URL", with: "example.com"
+      fill_in "Logo URL", with: "example-logo.com"
+      click_on "Add Airline"
+      expect(page).to have_content("Name has already been taken")
+    end
+  end
 
-        visit root_path
-        click_on "Create New Airline"
+  context "as a vistior" do
+    scenario "try to create an airline" do
+      visit root_path
+      click_on "Create New Airline"
+      click_on "Add Airline"
 
-        fill_in "Name", with: "Delta Airlines"
-        fill_in "Link URL", with: "example.com"
-        fill_in "Logo URL", with: "example-logo.com"
-        click_on "Add Airline"
-        expect(page).to have_content("Name has already been taken")
-      end
-   end
-
-   context "as a vistior" do
-       scenario "try to create an airline" do
-         visit root_path
-         click_on "Create New Airline"
-         click_on "Add Airline"
-
-         expect(page).to have_content("You need to sign in or sign up before continuing.")
-       end
-   end
-
+      expect(page).to have_content("You need to sign in or sign up before continuing.")
+    end
+  end
 end
