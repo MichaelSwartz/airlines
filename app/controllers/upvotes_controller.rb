3328e726d8e6483dcdda66afea5215b163d653b8
genagain <ohta.g@husky.neu.edu>
2015-04-06 11:23:24 -0400

    implemented upvote contraints

--- app/controllers/upvotes_controller.rb
+++ app/controllers/upvotes_controller.rb
@@ -1,15 +1,20 @@
 class UpvotesController < ApplicationController
 
   def create
-    @upvote = Vote.find_by(user_id: current_user[:id], review_id: params[:review_id])
     @airline = Review.find(params[:review_id]).airline
-    if @upvote
-      @upvote.value += 1
-      @upvote.save
-      flash[:notice] = "Upvote Submitted"
-    else
+    @upvote = Vote.find_by(user_id: current_user[:id], review_id: params[:review_id]) unless current_user.nil?
+    case true
+    when current_user.nil?
+      flash[:notice] = "You need to be logged in to vote!"
+    when @upvote.nil?
       @upvote = Vote.create(user_id: current_user[:id], review_id: params[:review_id], value: 1)
       flash[:notice] = "Upvote Created"
+    when @upvote.value == 1
+      flash[:notice] = "You've already upvoted!"
+    when
+      @upvote.value += 1
+      @upvote.save
+      flash[:notice] = "Upvote Submitted!"
     end
     redirect_to airline_path(@airline)
   end
