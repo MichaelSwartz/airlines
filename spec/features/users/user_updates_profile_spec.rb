d982ca9477e76427f61cf99fc766e441b0b72909
David Tsang <davidytsang@gmail.com>
2015-04-07 14:17:49 -0400

    completed testing for user profile photo

---
+++ spec/features/users/user_updates_profile_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+
+feature 'user updates profile', %Q{
+  As a user
+  I want to update my profile
+  so that I can change my email
+  and profile photo
+} do
+  let(:user) { FactoryGirl.create(:user) }
+
+  before :each do
+    sign_in_as user
+  end
+  scenario 'provide valid registration information with profile photo' do
+    click_link('Profile')
+    click_on('Update Account')
+
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    fill_in 'Current password', with: "#{user.password}"
+    attach_file "Profile photo", 'spec/fixtures/images.png'
+    click_button 'Update'
+
+    expect(page).to have_content('Your account has been updated successfully.')
+    click_link('Profile')
+    path = "uploads/user/profile_photo/#{user.id}/images.png"
+    expect(page).to have_xpath("//img[contains(@src,#{path})]")
+  end
+end
--- spec/features/users/user_uploads_profile_photo_spec.rb
+++ spec/features/users/user_uploads_profile_photo_spec.rb
@@ -1,31 +1,27 @@
-# require 'rails_helper'
-#
-# feature 'user registers', %Q{
-#   As a visitor
-#   I want to register
-#   So that I can create an account
-#   and upload a prifile photo
-# } do
-#
-#
-#   scenario 'provide valid registration information with profile photo' do
-#     visit new_user_registration_path
-#
-#     fill_in 'Email', with: 'john@example.com'
-#     fill_in 'Password', with: 'password'
-#     fill_in 'Password confirmation', with: 'password'
-#
-#     click_button 'Sign up'
-#
-#     expect(page).to have_content('Welcome! You have signed up successfully.')
-#     expect(page).to have_content('Sign Out')
-#   end
-#
-#   scenario 'provide invalid registration information' do
-#     visit new_user_registration_path
-#
-#     click_button 'Sign up'
-#     expect(page).to have_content("can't be blank")
-#     expect(page).to_not have_content('Sign Out')
-#   end
-# end
+require 'rails_helper'
+
+
+feature 'user registers', %Q{
+  As a visitor
+  I want to register
+  So that I can create an account
+  and upload a prifile photo
+} do
+
+  scenario 'provide valid registration information with profile photo' do
+    visit new_user_registration_path
+
+    fill_in 'Email', with: 'john@example.com'
+    fill_in 'Password', with: 'password'
+    fill_in 'Password confirmation', with: 'password'
+    attach_file "Profile photo", 'spec/fixtures/images.png'
+    click_button 'Sign up'
+
+    expect(page).to have_content('Welcome! You have signed up successfully.')
+    expect(page).to have_content('Sign Out')
+    click_link('Profile')
+    user = User.find_by(email: 'john@example.com' )
+    path = "uploads/user/profile_photo/#{user.id}/images.png"
+    expect(page).to have_xpath("//img[contains(@src,#{path})]")
+  end
+end
