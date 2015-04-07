6e251d9acb6c9f617a824668ec9bc6d396f71287
David Tsang <davidytsang@gmail.com>
2015-04-07 12:07:42 -0400

    completed profile picture, still requrie testing

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   root 'airlines#index'
   devise_for :users
-
+  resources :users
   resources :airlines do
     resources :reviews
   end
---
+++ spec/features/users/user_uploads_profile_photo_spec.rb
@@ -0,0 +1,31 @@
+# require 'rails_helper'
+#
+# feature 'user registers', %Q{
+#   As a visitor
+#   I want to register
+#   So that I can create an account
+#   and upload a prifile photo
+# } do
+#
+#
+#   scenario 'provide valid registration information with profile photo' do
+#     visit new_user_registration_path
+#
+#     fill_in 'Email', with: 'john@example.com'
+#     fill_in 'Password', with: 'password'
+#     fill_in 'Password confirmation', with: 'password'
+#
+#     click_button 'Sign up'
+#
+#     expect(page).to have_content('Welcome! You have signed up successfully.')
+#     expect(page).to have_content('Sign Out')
+#   end
+#
+#   scenario 'provide invalid registration information' do
+#     visit new_user_registration_path
+#
+#     click_button 'Sign up'
+#     expect(page).to have_content("can't be blank")
+#     expect(page).to_not have_content('Sign Out')
+#   end
+# end
