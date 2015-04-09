52a4a3279f31c9be9bb207ab5fde6f1c2351f442
genagain <ohta.g@husky.neu.edu>
2015-04-09 16:20:42 -0400

    Styled input fields and buttons on sign in, sign up and edit forms

--- spec/models/airline_spec.rb
+++ spec/models/airline_spec.rb
@@ -1,6 +1,10 @@
+require "rails_helper"
+
 describe "airline" do
-  it "should return zero when an airline has no reviews" do
-    airline = FactoryGirl.create(:airline)
-    expect(airline.average_rating).to eq 0
+  describe "#average_rating" do
+    it "should return zero when an airline has no reviews" do
+      airline = FactoryGirl.create(:airline)
+      expect(airline.average_rating).to eq 0
+    end
   end
 end
