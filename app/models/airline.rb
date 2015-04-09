e0c0da154e182494de254f04f87017cee150e636
genagain <ohta.g@husky.neu.edu>
2015-04-09 16:22:16 -0400

    fixed unit test

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -7,4 +7,17 @@ class Airline < ActiveRecord::Base
   def self.search(query)
     where("name ilike :q or description ilike :q", q: "%#{query}%")
   end
+
+  def average_rating
+    return 0 if reviews.empty?
+    sum = reviews.all.inject(0) do |total, review|
+      total += review.rating
+    end
+    count = reviews.count
+    sum.to_f / count
+  end
+
+  def self.sort_by_rating
+    all.sort { |x, y| y.average_rating <=> x.average_rating }
+  end
 end
