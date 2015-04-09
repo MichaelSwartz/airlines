ad3d23d63fc4b580db080b2cff7e580e479107fa
genagain <ohta.g@husky.neu.edu>
2015-04-09 14:11:02 -0400

    implemented average ratings and sorting

--- spec/features/airline_pagination_spec.rb
+++ spec/features/airline_pagination_spec.rb
@@ -2,18 +2,26 @@ require 'rails_helper'
 
 feature "paginating airlines" do
   scenario "user visits index and does not see 26th airline" do
-    FactoryGirl.create_list(:airline, 25)
+    24.times do
+      airline = FactoryGirl.create(:airline)
+      FactoryGirl.create(:review, airline: airline, rating: 3)
+    end
 
-    FactoryGirl.create(:airline, name: "second_page_air")
+    second_page = FactoryGirl.create(:airline, name: "second_page_air")
+    FactoryGirl.create(:review, airline: second_page, rating: 1)
     visit airlines_path
 
     expect(page).to_not have_content("second_page_air")
   end
 
   scenario "user clicks next to see 26th airline" do
-    FactoryGirl.create_list(:airline, 25)
+    24.times do
+      airline = FactoryGirl.create(:airline)
+      FactoryGirl.create(:review, airline: airline, rating: 3)
+    end
 
-    FactoryGirl.create(:airline, name: "second_page_air")
+    second_page = FactoryGirl.create(:airline, name: "second_page_air")
+    FactoryGirl.create(:review, airline: second_page, rating: 1)
 
     visit airlines_path
 
