0ea5fc08a6a8c29b1dc62b09ce75cd0ff0bf6455
Erik Grueter <erikgrueter@gmail.com>
2015-04-10 14:51:50 -0400

    added icon to pages

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -3,5 +3,9 @@ class HomesController < ApplicationController
 
   def index
     @airlines = Airline.limit(6)
+    if signed_in?
+      @user = User.find(current_user[:id])
+       @user_photo = @user[:profile_photo]
+    end
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -1,5 +1,4 @@
 require File.expand_path('../boot', __FILE__)
-
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
@@ -10,7 +9,6 @@ require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
 # require "rails/test_unit/railtie"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
